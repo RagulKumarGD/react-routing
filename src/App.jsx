ae9a2bd975f7f3ce2f8684d040d00df6827ffccf
RagulKumarGD. <ragulkumarkgm@gmail.com>
2024-11-26 17:09:08 +0530

    se

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
        
     }
     (async()=>await fetchh())()
-  },[])
+  })
   // useEffect(()=>{
   //   const fetchh=async()=>{
   //     const respone=await api.get('/items')
